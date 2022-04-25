9308c6586e98bddeca6b593819f71739a8b2a31b
not4Pedro <peto.leitmann@gmail.com>
2022-04-25 19:36:58 +0200

    Integer and buffer overflow

--- hello_world.c
+++ hello_world.c
@@ -1,82 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+int target = 0;
+
+void func()
 {
-   int num;
+   printf("Integer overflow \n");
+   exit(EXIT_FAILURE);
+}
+
+void bufferOverflow() { printf("Buffer Overflow\n"); }
+
+void stringfO(char *string){
+   if(target){
+      printf("Data reached\n");
+   }
+}
+
+int main(int argc, char **argv)
+{
+   int test = 0;
+   char buffer[10];
+   // strcpy(buffer, argv[1]);
+
+   stringfO(argv[1]);
+
    int numbers[500];
-  int i = 0;
+   int i = 0;
    FILE *fptr;
 
-   // use appropriate location if you are using MacOS or Linux
-   fptr = fopen("test.txt","a");
-
-   if(fptr == NULL)
+   scanf("%s", buffer);
+   if (test)
    {
-      printf("Error!");   
-      exit(1);             
+      bufferOverflow();
+   }
+
+   // use appropriate location if you are using MacOS or Linux
+   fptr = fopen(buffer, "w");
+
+   if (fptr == NULL)
+   {
+      printf("Error!");
+      exit(1);
    }
    if (argc == 2)
    {
-      if(argv[1] > 5) fprintf(fptr, "%s \n", argv[1]);
+      if (argv[1] > 1)
+      {
+         fprintf(fptr, "%s \n", argv[1]);
+      }
    }
-   // else {   
-   //    printf(stdout, "Usage %s <name>", argv[0]);
-   //    return 1;
-   // }
-   
-
-   // printf("Enter num: ");
-   // scanf("%d",&num);
-
-   
-   // fprintf(fptr,"%d",num);
-   // fclose(fptr);
 
    printf("Start of the app \n");
 
-   fprintf(fptr,"%d\n", 2599);
-   fprintf(fptr,"%d\n", 454);
-   fprintf(fptr,"%d\n", 45);
-   fprintf(fptr,"%d\n", 97);
-   fprintf(fptr,"%d\n", 14154);
-
+   fprintf(fptr, "%d\n", 1);
+   fprintf(fptr, "%d\n", 2);
+   fprintf(fptr, "%d\n", 3);
+   fprintf(fptr, "%d\n", 4);
+   fprintf(fptr, "%d\n", 5);
+   
    fclose(fptr);
+   
+   //fptr = fopen(buffer, "r");
 
-   // READING FILE
-   // if ((fptr = fopen("test.txt","r")) == NULL){
-   //     printf("Error! opening file");
+   if (fptr = fopen(buffer, "r"))
+   {
+   printf( "test\n");
+      while (fscanf(fptr, "%d", &numbers[i]) != EOF)
+      {
+         printf("%d", numbers[i]);
+         if (numbers[i] < 0)
+         {
+            func();
+         }
+         i++;
+      }
+      fclose(fptr);
 
-   //     // Program exits if the file pointer returns NULL.
-   //     exit(1);
-   // }
+      printf("",buffer);
 
-   // char str1 = fgetc(fptr);
-	// while (str1 != EOF)
-	// 	{
-   //       if (((int)str1) == -1) printf("INTEGER OWERFLOW");
-         
-	// 		printf ("%c", str1);
-	// 		str1 = fgetc(fptr);
-	// 	}
-   // fscanf(fptr,"%d", &num);
+      numbers[i] = '\0';
 
-   // printf("Value of n=%d", num);
-   // fclose(fptr); 
+      for (i = 0; numbers[i] != '\0'; i++)
+      {
 
-   if (fptr = fopen("test.txt", "r"))
-  {
-    while (fscanf(fptr, "%d", &numbers[i]) != EOF)
-    {
-      i++;
-    }
-    fclose(fptr);
-
-    numbers[i] = '\0';
-
-    for (i = 0; numbers[i] != '\0'; i++)
-      printf("%d\n", numbers[i]);
-  }
+         printf("%d\n", numbers[i]);
+      }
+   }
 
    return 0;
 }
